27d95082517464d7f80615e646ba03bb4ef1c26f
rjman <rjman.ljm@gmail.com>
2022-12-27 12:49:41 +0800

    add jest config for keystore package (#2)
    * add jest config for keystore package
    
    * fix .gitignore
    
    * udpate keystore package.json
    
    * upload missing files
    
    * fix chore

--- packages/keystore/jest.config.js
+++ packages/keystore/jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  verbose: true,
+  collectCoverage: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    "ts-jest": {
+      "packageJson": "../../package.json"
+    }
+  },
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+    "lib/index.ts"
+  ]
 };
---
+++ packages/keystore/lib/errors.ts
@@ -0,0 +1,4 @@
+export const ErrorInvalidAddress = "invalid address";
+export const ErrorKeyFileNotExist = "the key file does not exist";
+export const ErrorInvalidPassword = "invalid password";
+export const ErrorInvalidPK = "invalid pk";
