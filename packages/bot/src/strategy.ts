501635b66993b41bf9617a296d66afdfd1b88cc8
rjman <rjman.ljm@gmail.com>
2022-12-28 18:53:51 +0800

    update: compound stratgy for fork mainnet

--- packages/bot/src/strategy.ts
+++ packages/bot/src/strategy.ts
@@ -14,9 +14,9 @@ export const strategy: NetworkConfiguration<Recordable<string>> = {
         MAYC_TOKEN_PENDING_REWARD_LIMIT: "50",
     },
     fork_mainnet: {
-        BAYC_USER_PENDING_REWARD_LIMIT: "60",
-        BAYC_TOKEN_PENDING_REWARD_LIMIT: "55",
-        MAYC_USER_PENDING_REWARD_LIMIT: "18",
-        MAYC_TOKEN_PENDING_REWARD_LIMIT: "15",
+        BAYC_USER_PENDING_REWARD_LIMIT: "10",
+        BAYC_TOKEN_PENDING_REWARD_LIMIT: "5",
+        MAYC_USER_PENDING_REWARD_LIMIT: "10",
+        MAYC_TOKEN_PENDING_REWARD_LIMIT: "5",
     },
 };
