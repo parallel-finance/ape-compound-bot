25a076f36b84de30efb89eb0314b729a9e017bba
jfzhou5 <1241330802@qq.com>
2023-05-09 22:22:46 +0800

    fix: update feeToEth

--- packages/bot/src/feeToEth.ts
+++ packages/bot/src/feeToEth.ts
@@ -6,8 +6,9 @@ import { Alert, logger, mapErrMsg, toEtherscanLink } from "@para-space/utils"
 
 const APE_WETH_FEE = "3000"
 export const swapApeFeeToETH = async () => {
+    logger.info("start swap ape fee to eth.")
     const uniswapV3Quoter = runtime.provider.connectContract(ParaSpaceEthMM.UniswapV3Quoter)
-    const uniswapV3: Types.IUniSwapV3Router02 = runtime.provider.connectContract(
+    const uniswapV3Router: Types.IUniSwapV3Router02 = runtime.provider.connectContract(
         ParaSpaceEthMM.UniswapV3Router,
         runtime.wallet
     )
@@ -16,19 +17,29 @@ export const swapApeFeeToETH = async () => {
         ["address", "uint24", "address"],
         [ERC20.APE, APE_WETH_FEE, ERC20.WETH]
     )
-    const amountIn = await runtime.provider
-        .connectFactory(Factories.ERC20__factory, ERC20.APE)
-        .balanceOf(runtime.wallet.address)
+    const ape: Types.ERC20 = runtime.provider.connectFactory(
+        Factories.ERC20__factory,
+        ERC20.APE,
+        runtime.wallet
+    )
+    const amountIn = (await ape.balanceOf(runtime.wallet.address)).div(2)
+
     try {
+        if ((await ape.allowance(runtime.wallet.address, uniswapV3Router.address)).lt(amountIn)) {
+            const tx = await ape.approve(uniswapV3Router.address, ethers.constants.MaxUint256)
+            await tx.wait()
+            logger.info(`ape bot approve ape to uniswapV3Router, tx: ${tx.hash}`)
+        }
         const amountOut = await uniswapV3Quoter.callStatic.quoteExactInput(apeWethPath, amountIn)
         const exactInputParams = {
             path: apeWethPath,
             recipient: runtime.wallet.address,
             amountIn,
-            amountOutMinimum: amountOut.mul(99).div(100)
+            amountOutMinimum: amountOut.mul(9900).div(10000)
         }
-        const gas = await uniswapV3.estimateGas.exactInput(exactInputParams)
-        const tx = await uniswapV3.exactInput(exactInputParams, {
+        // console.log(uniswapV3Router.interface.encodeFunctionData("exactInput", [exactInputParams]))
+        const gas = await uniswapV3Router.estimateGas.exactInput(exactInputParams)
+        const tx = await uniswapV3Router.exactInput(exactInputParams, {
             gasLimit: gas.add("10000")
         })
         await tx.wait()
@@ -61,13 +72,14 @@ export const swapApeFeeToETH = async () => {
             ])
         }
     } catch (e) {
+        const errMsg = `Do swap AC fee to ETH error: ${mapErrMsg(e)}`
+        logger.error(errMsg)
         if (runtime.slack.enable) {
-            const errMsg = `Do swap AC fee to ETH error: ${mapErrMsg(e)}`
-            logger.error(errMsg)
             Alert.error(errMsg, [
                 { name: "network", value: runtime.networkName },
                 { name: "error", value: (e as any).toString() }
             ])
         }
     }
+    logger.info("end swap ape fee to eth.")
 }
