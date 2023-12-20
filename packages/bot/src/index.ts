e7b11eeb7f7e3224431f9fb66c6013563523b311
jfzhou5 <1241330802@qq.com>
2023-12-20 20:17:19 +0800

    stop v1 AC

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -5,23 +5,22 @@ import { fetchCompoundInfo } from "./supplyAndStake/fetch"
 import { fetchP2PCompoundInfo } from "./p2p/fetch"
 import { claimAndCompoundForP2PPairStaking } from "./p2p/compound"
 import { SimpleMatchOrder, ValidCompoundInfo } from "./types"
-import { swapApeFeeToETH } from "./feeToEth"
 
 async function main() {
     logger.info("Starting paraspace-ape-compound-bot")
 
     const worker = async () => {
-        const v1CompoundInfo: ValidCompoundInfo = await fetchCompoundInfo(true)
-        await claimAndCompound(true, v1CompoundInfo)
-        const v1P2pCompoundInfo: SimpleMatchOrder[] = await fetchP2PCompoundInfo(true)
-        await claimAndCompoundForP2PPairStaking(true, v1P2pCompoundInfo)
+        // const v1CompoundInfo: ValidCompoundInfo = await fetchCompoundInfo(true)
+        // await claimAndCompound(true, v1CompoundInfo)
+        // const v1P2pCompoundInfo: SimpleMatchOrder[] = await fetchP2PCompoundInfo(true)
+        // await claimAndCompoundForP2PPairStaking(true, v1P2pCompoundInfo)
 
         const v2CompoundInfo: ValidCompoundInfo = await fetchCompoundInfo(false)
         await claimAndCompound(false, v2CompoundInfo)
         const v2P2pCompoundInfo: SimpleMatchOrder[] = await fetchP2PCompoundInfo(false)
         await claimAndCompoundForP2PPairStaking(false, v2P2pCompoundInfo)
 
-        await swapApeFeeToETH()
+        // await swapApeFeeToETH()
     }
     await Runtime.run(worker)
 
